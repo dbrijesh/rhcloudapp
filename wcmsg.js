337734889bae1bf5c8ad1fcafb9f51539d58eb1e
dbrijesh <dbrijesh@gmail.com>
2014-03-05 21:42:49 +0530

    Create wcmsg.js

---
+++ wcmsg.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("This is Brijesh's app page\n");
+});
+
+// Listen on port 8000
+server.listen(8000);
